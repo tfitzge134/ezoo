ab3be1319fbd5df4452108f82f40565430d13b09
TERESA Hidalgo De Fitzgerald <tfitzge134@gmail.com>
2021-04-08 17:12:32 -0500

    completed dive 3

--- eZoo/src/TestFeedingSchedules.java
+++ eZoo/src/TestFeedingSchedules.java
@@ -6,17 +6,79 @@ import com.ezoo.feedingschedules.dao.FeedingScheduleDAOImpl;
 import com.feeding_schedules.model.FeedingSchedule;
 
 public class TestFeedingSchedules {
-	public static void main(String[] args) throws BusinessException{
-	    FeedingScheduleDAO dao = new FeedingScheduleDAOImpl();
-	    List<FeedingSchedule> list = dao.getAllFeedingSchedule();
+	public static void main(String[] args) {
+		
+		listFeedingSchedules();
+		System.out.println("----------");
+		assignFeedingSchedule();
+		System.out.println("----------");
+		listFeedingSchedules();
+		 System.out.println("-----------");
+		 deleteFeedingSchedules();
+		 System.out.println("-----------");
+		 removeFeedingSchedule();
+		 getFeedingSchedule();
+		
+	}
 
-	    for (int i = 0; i < list.size(); i++){
-	      FeedingSchedule f = list.get(i);
-	      System.out.println(f);
-	    }
-	  }	
+	private static void assignFeedingSchedule() {
+		FeedingScheduleDAO dao = new FeedingScheduleDAOImpl();
+		try {
+			FeedingSchedule feedingScheduleX= 
+					new FeedingSchedule(10, "Afternoon", "morning", "AAA", "nnn", 2);
+			dao.addFeedingSchedule(feedingScheduleX);
+			System.out.println("---DONE: assignFeedingSchedule");
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void deleteFeedingSchedules() {
+		FeedingScheduleDAO dao1 = new FeedingScheduleDAOImpl();
+		try {
+			dao1.deleteById(1);
+			System.out.println("---DONE: deleteById");
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listFeedingSchedules() {
+		FeedingScheduleDAO dao = new FeedingScheduleDAOImpl();
+		List<FeedingSchedule> list;
+		try {
+			list = dao.getAllFeedingSchedule();
+			for (int i = 0; i < list.size(); i++) {
+				FeedingSchedule f = list.get(i);
+				System.out.println(f);
+			}
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	  private static void removeFeedingSchedule() {
+		FeedingScheduleDAO dao1 = new FeedingScheduleDAOImpl();
+		try {
+			dao1.removeFeedingSchedule(2);
+			System.out.println("---DONE: animal_id");
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	  private static void getFeedingSchedule() {
+			FeedingScheduleDAO dao = new FeedingScheduleDAOImpl();
+			try {
+
+				dao.getFeedingSchedule(3);
+				System.out.println("---DONE: assignFeedingSchedule");
+			} catch (BusinessException e) {
+				e.printStackTrace();
+			}
+		}  
 
 }
-
 
 
